dda7994fa717b99409f51c9dcb4091ce446be3fb
Gahie Okou <gokou097@gmail.com>
2023-03-01 14:31:16 +0000

    base url change

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: `https://${process.env.VERCEL_URL}/api`,
+  baseURL: `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
 });
