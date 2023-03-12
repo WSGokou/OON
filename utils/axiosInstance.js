ed41b75079914ecc86c5f8ba0cb669a7f727f8c3
Gahie Okou <gokou097@gmail.com>
2023-03-12 18:02:26 +0000

    headers added for cors

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -2,4 +2,17 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+  headers: [
+    {key: 'Access-Control-Allow-Credentials', value: 'true'},
+    {key: 'Access-Control-Allow-Origin', value: '*'},
+    {
+      key: 'Access-Control-Allow-Methods',
+      value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    },
+    {
+      key: 'Access-Control-Allow-Headers',
+      value:
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    },
+  ],
 });
